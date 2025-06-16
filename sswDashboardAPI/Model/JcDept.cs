03189b0e2b62a5914de430b43d7ff1785b2cd0e8
akashsswhite <akashsharma@sswhite.net>
2025-06-16 16:18:33 -0400

    jcdept1 invalid col name fixed

--- sswDashboardAPI/Model/JcDept.cs
+++ sswDashboardAPI/Model/JcDept.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace sswDashboardAPI.Model
 {
     public class JcDept
     {
         [Key]
+        [Column("JCDept")]
         public string JCDept1 { get; set; }
         public string Description { get; set; }
         public string Company { get; set; }
